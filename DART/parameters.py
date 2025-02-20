d6d76deb9863774bbb2d2a5f43e3838760764496
Alexis Burgon <88703431+alexisburgon@users.noreply.github.com>
2025-02-20 08:42:07 -0800

    Added docstrings

--- DART/parameters.py
+++ DART/parameters.py
@@ -7,11 +7,24 @@ from collections.abc import MutableMapping
 import pprint
 
 class Parameters(MutableMapping, dict):
+    """
+    Manages package-wide parameters.
+
+    Parameters
+    ----------
+    **kwargs
+        Specified parameter values. Default values will be used for any parameter not specified.
+
+    Notes
+    -----
+    :class:`parameters` is a global version of :class:`Parameters` with all default values.
+    """
     default = {
         "numeric.ideal_bin_count": [5,10],
         "hypervectors.dimensions": 10000,
         "hypervectors.architecture": "MAP",
     }
+    supported_architectures = ["MAP"]
 
     def __init__(self, **kwargs):
         """ Initializes a copy with the default parameters and then sets any specified values """
@@ -25,10 +38,14 @@ class Parameters(MutableMapping, dict):
         """ Directly sets value without any checks. """
         dict.__setitem__(self, key, val)
 
-    def __setitem__(self, key, val): # TODO [WIP]
+    def __setitem__(self, key, val):
         if key not in self.default:
             raise Exception(f"Unrecognized parameter \"{key}\"")
-        # TODO: validate different parameters
+        if key == "hypervectors.architecture" and val not in self.supported_architectures:
+            raise Exception(f"The architecture \"{val}\" is not currently supported.")
+        if key == "hypervectors.dimensions" and val < 10000:
+            print(f"Warning: it is recommended that the hypervector dimensionality be >=10,000. Using dimensionality \"{val}\" may produce unexpected results.")
+        # TODO: improve parameter validation
         self._set(key, val)
     
     def __getitem__(self, key):
