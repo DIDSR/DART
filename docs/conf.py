8cabb72a264da6530bee3dca4eb8ec386c80e7ba
Alexis Burgon <88703431+alexisburgon@users.noreply.github.com>
2025-02-20 09:43:08 -0800

    Added project description and GitHub link to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,14 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
+html_theme_options = { # options available depend on the theme used
+    "github_user": "DIDSR",
+    "github_repo": "DART",
+    "description": "Quantitative DAta RepresenTativeness measurment"
+}
+
 html_static_path = ['_static']
